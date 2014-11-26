9f459d1f10d76ced7b2a43059d9e6d1bbc0c8052
braican <nick.braica@gmail.com>
2014-11-26 16:09:44 -0500

    googla analytics

--- webroot/wp-content/themes/breakbrewing/header.php
+++ webroot/wp-content/themes/breakbrewing/header.php
@@ -12,6 +12,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+<link rel="canonical" href="http://breakbrewing.com" />
+
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
